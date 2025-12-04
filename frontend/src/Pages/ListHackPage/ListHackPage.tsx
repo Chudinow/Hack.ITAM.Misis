56c8a57c67a55728a70e03baaaca34fa68722759
Федярин Максим <maksonchik@MacBookMaksonchik.local>
2025-12-05 00:07:43 +0300

    добавляет стиль на ListHack

--- frontend/src/Pages/ListHackPage/ListHackPage.tsx
+++ frontend/src/Pages/ListHackPage/ListHackPage.tsx
@@ -1,9 +1,53 @@
-const ListHackPage = () =>{
-    return(
-        <div>
-        hello world ListHackPage
-        </div>
-    )
-}
+import React from "react";
+import styles from "./list-hack-page.module.css";
+import { Link } from "react-router-dom";
+import exampleImg from "../../Photos/1.png";
 
-export default ListHackPage
+const ListHackPage: React.FC = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Ближайшие Хакатоны</h1>
+      
+      <div className={styles.cards}>
+        
+        <Link to="../hackdetails" className={styles.card}>
+          <img src={exampleImg} alt="hack-img" className={styles.image} />
+
+          <div className={styles.cardContent}>
+            <h2 className={styles.cardTitle}>Название</h2>
+            <p className={styles.cardText}>
+              описание крч оч интересное все такое классное реально респект
+              всем всех люблю уважаю поддерживаю ну или что-то другое
+            </p>
+          </div>
+        </Link>
+
+        <Link to="../hackdetails" className={styles.card}>
+          <img src={exampleImg} alt="hack-img" className={styles.image} />
+
+          <div className={styles.cardContent}>
+            <h2 className={styles.cardTitle}>Название</h2>
+            <p className={styles.cardText}>
+              описание крч оч интересное все такое классное реально респект
+              всем всех люблю уважаю поддерживаю ну или что-то другое
+            </p>
+          </div>
+        </Link>
+
+        <Link to="../hackdetails" className={styles.card}>
+          <img src={exampleImg} alt="hack-img" className={styles.image} />
+
+          <div className={styles.cardContent}>
+            <h2 className={styles.cardTitle}>Название</h2>
+            <p className={styles.cardText}>
+              описание крч оч интересное все такое классное реально респект
+              всем всех люблю уважаю поддерживаю ну или что-то другое
+            </p>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ListHackPage;
--- frontend/src/global.d.ts
+++ frontend/src/global.d.ts
@@ -6,3 +6,7 @@ declare module '*.module.scss' {
   const classes: { [key: string]: string };
   export default classes;
 }
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";

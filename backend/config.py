b9ecebae700d1f88374dcfc9f5487d696043e2f2
Chudinow <77829668+Chudinow@users.noreply.github.com>
2025-12-07 17:20:25 +0300

    исправил config

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,4 @@
+
 from decouple import config
 from dotenv import load_dotenv
 
@@ -14,3 +15,25 @@ DB_PASSWORD = config("DB_PASSWORD")
 SECRET_KEY = config("SECRET_KEY")
 JWT_EXPIRE_MINUTES = config("JWT_EXPIRE_MINUTES", cast=int, default=1440)
 TG_BOT_TOKEN = config("TG_BOT_TOKEN")
+
+"""--------------------------------------------"""
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# БД
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = int(os.getenv("DB_PORT", 5432))
+DB_USERNAME = os.getenv("DB_USERNAME", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+DB_NAME = os.getenv("DB_NAME", "hackathon")
+
+# API
+API_PREFIX = os.getenv("API_PREFIX", "/api/v1")
+
+# JWT
+SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key-change-in-production")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
